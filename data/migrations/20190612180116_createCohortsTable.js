77e3a5eae6f49ab75bfbce62013d5d095c32afb2
shanreed <srtech5set@gmail.com>
2019-06-12 19:20:20 -0700

    added cohorts and students tables and seeds cleanup file

--- data/migrations/20190612180116_createCohortsTable.js
+++ data/migrations/20190612180116_createCohortsTable.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex, Promise) {
+    return knex.schema.createTable('cohorts', (tbl) => {
+        tbl.increments(); 
+        tbl
+        .string('name', 128) 
+        .notNullable()
+    
+        tbl.timestamps(true, true) 
+      })
+
   
 };
 
 exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('cohorts')
   
 };
